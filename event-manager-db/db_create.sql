81df7918396c6ce7ccf4c163dd82511687aa6df8
Anthony <anthony.olea@etudiant.mines-nantes.fr>
2014-10-29 23:09:40 +0100

    Updating SQL script

--- event-manager-db/db_create.sql
+++ event-manager-db/db_create.sql
@@ -1,29 +1,32 @@
---DROP TABLE event;
---DROP TABLE customer;
---DROP TABLE participant;
+--DROP TABLE app.event;
+--DROP TABLE app.customer;
+--DROP TABLE app.participant;
 
-CREATE TABLE event (
-    event_id varchar(32) NOT NULL PRIMARY KEY,
-    event_name varchar(50) NOT NULL,
-    event_location varchar(50) NOT NULL,
-    event_start_datetime bigint NOT NULL,
-    event_end_datetime bigint NOT NULL,
-    event_creator bigint NOT NULL
-);
-
-CREATE TABLE customer (
+CREATE TABLE app.customer (
     customer_id bigint NOT NULL PRIMARY KEY,
     customer_email varchar(254) NOT NULL UNIQUE,
     customer_password varchar(100),
     customer_firstname varchar(50) NOT NULL,
     customer_lastname varchar(50),
-    customer_company varchar(50), 
+    customer_company varchar(50)
 );
 
-CREATE TABLE participant (
-    event_id varchar(32) NOT NULL,
-    customer_id bigint NOT NULL,
-    CONSTRAINT fk_participant_event FOREIGN KEY (event_id) REFERENCES event(event_id),
-    CONSTRAINT fk_participant_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    CONSTRAINT participant_id PRIMARY KEY (event_id, customer_id)
+CREATE TABLE app.event (
+    event_id varchar(32) NOT NULL PRIMARY KEY,
+    event_name varchar(50) NOT NULL,
+    event_location varchar(50) NOT NULL,
+    event_start_datetime timestamp NOT NULL,
+    event_end_datetime timestamp NOT NULL,
+    event_creator_id bigint NOT NULL
 );
+
+CREATE TABLE app.participant (
+    participant_event_id varchar(32) NOT NULL,
+    participant_customer_id bigint NOT NULL,
+    CONSTRAINT fk_participant_event FOREIGN KEY (participant_event_id) REFERENCES app.event(event_id),
+    CONSTRAINT fk_participant_customer FOREIGN KEY (participant_customer_id) REFERENCES app.customer(customer_id),
+    CONSTRAINT participant_id PRIMARY KEY (participant_event_id, participant_customer_id)
+);
+
+--ALTER TABLE app.event
+--ADD CONSTRAINT fk_event_creator FOREIGN KEY (event_creator_id) REFERENCES app.customer(customer_id);
