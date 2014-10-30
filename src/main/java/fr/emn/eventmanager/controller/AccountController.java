d3847e28743c08150611b75894ba36c436ca596d
antoinebovin <antoine.bovin@gmail.com>
2014-10-30 18:33:17 +0100

    RegisterView

--- src/main/java/fr/emn/eventmanager/controller/AccountController.java
+++ src/main/java/fr/emn/eventmanager/controller/AccountController.java
@@ -39,6 +39,7 @@ public class AccountController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	System.out.println(request.getParameter("passwordRegister"));
 	}
 	
 }
